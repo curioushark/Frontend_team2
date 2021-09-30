171c0cbfbbfea75572098b064365b178163927c9
Ghina D <90589117+curioushark@users.noreply.github.com>
2021-09-30 22:11:57 +0700

    Update AccCust.ts

--- AccCust.ts
+++ AccCust.ts
@@ -1,21 +1,24 @@
 export class AccCust{
 
-  id:string;
-
-  nik:number;
-
-  fullname:string;
-
-  email:string;
-
-  address:string;
-
-  accNumber:number;
-
-  pin:number;
-
-  balance:number;
-
- }
-
-
+    id:string;
+  
+    nik:number;
+  
+    fullname:string;
+  
+    email:string;
+  
+    address:string;
+  
+    accNumber:number;
+  
+    pin:number;
+  
+    balance:number;
+  
+    accId:string;
+  
+    amount:number;
+  
+    toFrom:string;
+   }
